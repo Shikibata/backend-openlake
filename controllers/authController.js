0769575e04fc7be66478fe82f7dc9081ee41eb88
MaximeHoll <maxime.hollanders15@gmail.com>
2023-01-27 15:00:56 +0100

    trying to fix logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -111,9 +111,11 @@ const refresh = (req,res) => {
 
 const logout = (req,res) => {
     const cookies = req.cookies
-    if (!cookies?.jwt) return res.sendStatus(204) //No content
-    res.clearCookie('openlake', { httpOnly: true, sameSite: 'None', secure: true })
-    res.json({message: 'Cookie cleared'})
+    // if (!cookies?.jwt) return res.sendStatus(204) //No content
+    // res.clearCookie('openlake', { httpOnly: true, sameSite: 'None', secure: true })
+    // res.json({message: 'Cookie cleared'})
+    res.status(202).clearCookie('openlake').send('cookie cleared')
+
 }
 
 module.exports = {

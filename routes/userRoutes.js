cf174a6859dba9e252d2113f892010b6a1fea56b
MaximeHoll <maxime.hollanders15@gmail.com>
2023-01-27 16:28:08 +0100

    fixed middleware users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const usersController = require('../controllers/usersController')
 const {verifyJWTAdmin, verifyJWT} = require('../middleware/verifyJWT')
 
-router.use(verifyJWTAdmin)
+
 
 router.route('/')
-    .get(usersController.getAllUsers)
+    .get(verifyJWTAdmin,usersController.getAllUsers)
     .post(usersController.createNewUser)
     .patch(usersController.updateUser)
-    .delete(usersController.deleteUser)
+    .delete(verifyJWTAdmin,usersController.deleteUser)
 
 module.exports = router
